7f46b664a1a0d90d71131d1352655b0194733c67
Yonaim <yonaimyo@gmail.com>
2026-05-12 07:39:49 +0900

    필요없는 파일 삭제

--- LunaticEngine/Source/Editor/Common/UI/Docking/DockLayoutUtils.cpp
+++
@@ -1,3 +0,0 @@
-#include "Common/UI/Docking/DockLayoutUtils.h"
-
-// FDockLayoutUtils는 현재 의도적으로 헤더 전용으로 유지한다.
--- LunaticEngine/Source/Editor/Common/UI/Panels/Panel.cpp
+++
@@ -1,4 +0,0 @@
-#include "Common/UI/Panels/Panel.h"
-
-// FPanel은 현재 의도적으로 헤더 전용으로 유지한다.
-// 이 cpp 파일은 프로젝트 필터에 일관되게 포함시키기 위해 존재한다.
